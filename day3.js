536730f8b28030b3b3ce42e2485d1d3b4634888f
Turaab Bajeer <50545053+turab45@users.noreply.github.com>
2020-12-08 19:39:14 +0500

    Create day3.js

---
+++ day3.js
@@ -0,0 +1,16 @@
+/*
+****************** CHALLANGE LINK: https://www.hackerrank.com/challenges/js10-try-catch-and-finally/problem
+
+*/
+
+
+
+function reverseString(s) {
+    try {
+        s = s.split("").reverse().join("");
+    } catch(e) {
+        console.log(e.message);
+    } finally {
+        console.log(s);
+    }
+}
